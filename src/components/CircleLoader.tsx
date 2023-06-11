7a79ef086157b69af870f0b010f752673511a079
Tibor Fazekas <tibcso0322@gmail.com>
2023-06-11 14:35:40 +0200

    added img etc

--- src/components/CircleLoader.tsx
+++ src/components/CircleLoader.tsx
@@ -1,4 +1,4 @@
-const CircleLoader = ({ className }: { className?: string  }) => {
+const CircleLoader = () => {
   return (
     <div className="flex justify-center items-center h-full">
       <div className="w-10 h-10 rounded-full animate-pulse"></div>
